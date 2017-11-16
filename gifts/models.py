cfcc30d7577e01f81b5a28a3d26b8af30e5aa34b
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-16 14:11:07 -0500

    created models and switched db to postgresql

--- gifts/models.py
+++ gifts/models.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Gift(models.Model):
+    name = models.CharField(max_length=30)
+    price_in_cents = models.IntegerField()
+    sku = models.IntegerField()
+
+
+class Wishlist(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    gifts = models.ManyToManyField(Gift)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birthday = models.DateField()
+    friends = models.ManyToManyField("self")
