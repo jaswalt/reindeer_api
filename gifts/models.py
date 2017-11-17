ce8424dd03b0e2aea2a44d79434c96d8baf2f9be
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-16 19:56:54 -0500

    Created a user's gifts index json route

--- gifts/models.py
+++ gifts/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -8,6 +8,7 @@ class Gift(models.Model):
     name = models.CharField(max_length=30)
     price_cents = models.IntegerField()
     sku = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -17,6 +18,7 @@ class Wishlist(models.Model):
     title = models.CharField(max_length=255)
     date = models.DateField()
     gifts = models.ManyToManyField(Gift)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- gifts/views.py
+++ gifts/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+from users.models import User
 
 # Create your views here.
 def index(request, user_id):
-    return HttpResponse(f"Hello, {user_id}")
+    gifts = User.objects.get(pk=user_id).gift_set.all()
+    gifts = serializers.serialize('json', gifts)
+    return JsonResponse(gifts, safe=False)
