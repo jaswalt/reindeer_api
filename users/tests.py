49eb1d0c94e1a358bf9d183294c7858a00d72438
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-17 13:24:14 -0500

    test user creation

--- users/tests.py
+++ users/tests.py
@@ -6,6 +6,19 @@ from .models import User
 
 class UserModelTests(TestCase):
 
+    def setUp(self):
+        self.user = User.objects.create(
+            username = 'johndoe', 
+            password = 'testing',
+            first_name = 'John', 
+            last_name = 'Doe',
+            email = 'test@testing.com',
+            dob = '1999-09-09',
+        )
+    
+
     def test_string_representation(self):
-        user = User(first_name="TestName")
-        self.assertEqual(str(user), user.first_name)
+        self.assertEqual(str(self.user), self.user.first_name)
+    
+    def test_user_creation(self):
+        self.assertIsInstance(self.user, User)
