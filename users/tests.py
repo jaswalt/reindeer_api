be112fb69dd472fcb64b37540b3a05b4a8991bf5
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-17 11:37:58 -0500

    unit test for user model

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+from .models import User
+
 # Create your tests here.
+
+class UserModelTests(TestCase):
+
+    def test_string_representation(self):
+        user = User(first_name="TestName")
+        self.assertEqual(str(user), user.first_name)
