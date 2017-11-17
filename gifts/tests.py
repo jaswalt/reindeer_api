89eb103d77b8d96530b19efaa0ce279aa9d69941
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-11-17 13:55:43 -0500

    beginning to do tests for gifts

--- gifts/tests.py
+++ gifts/tests.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+import unittest
+
+from models import Gift
+from models import Wishlist
 
 # Create your tests here.
+
+class TestGiftModel(TestCase)
+
+def setUp(self):
+    gift = Gift(name="Bob Dob")
+    wishlist = Wishlist(title="BEST LIST EVER", date="09022018")
+
+def test_gift(self):
+    self.assertInstance()
+
+ 
