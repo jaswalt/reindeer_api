23f36766c41933f23fd5e64d4dad098118a8b34d
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-16 19:14:55 -0500

    Added gifts index route

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^([0-9]+)$', views.show),
+    url(r'^([0-9]+)/gifts/', include('gifts.urls'))
 ]
