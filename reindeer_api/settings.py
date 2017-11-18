43ac863110c0a8244ed9ebacb3c2aa4a4bc7c441
Lex Yehia <lex.yehia@icloud.com>
2017-11-18 00:27:54 -0500

    Added color to test runner in Terminal, and added password encryption tests to users test

--- reindeer_api/settings.py
+++ reindeer_api/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'reindeer_api.wsgi.application'
 
+TEST_RUNNER = 'redgreenunittest.django.runner.RedGreenDiscoverRunner'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
@@ -91,6 +92,14 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+]
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- users/tests.py
+++ users/tests.py
@@ -1,26 +1,32 @@
 from django.test import TestCase
-
 from .models import User, UserForm
 
 
 class UserModelTests(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create(
-            username = 'johndoe', 
-            password = 'testing',
-            first_name = 'John', 
-            last_name = 'Doe',
-            email = 'test@testing.com',
-            dob = '1999-09-09',
+        self.user = User(
+            username='johndoe',
+            first_name='John',
+            last_name='Doe',
+            email='test@testing.com',
+            dob='1999-09-09',
         )
-    
+        self.user.set_password('testing')
+        self.user.save()
+
     def test_string_representation(self):
         self.assertEqual(str(self.user), self.user.first_name)
-    
+
     def test_user_creation(self):
         self.assertIsInstance(self.user, User)
 
     def test_username_uniqueness(self):
         user_form = UserForm({'username': 'johndoe'})
         self.assertEqual(user_form.errors['username'], ['A user with that username already exists.'])
+    
+    def test_password_should_not_be_plain_text(self):
+        self.assertNotEqual(self.user.password, 'testing')
+
+    def test_password_should_be_decodable(self):
+        self.assertTrue('testing', self.user.password)
