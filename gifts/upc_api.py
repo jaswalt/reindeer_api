3c9dace7c91cf23f73c3d4daad13c71e1ae8f2da
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-11-20 19:47:52 -0500

    retrieved walmart product data

--- gifts/upc_api.py
+++ gifts/upc_api.py
@@ -1,10 +1,13 @@
 from urllib.parse import urlparse
-import httplib2 as http
 import json
+import httplib2 as http
+from django.utils.html import strip_tags
+import html.parser
+html_parser = html.parser.HTMLParser()
 
 class ProductInfo:
 
-    
+
     @classmethod
     def fetch_product_info(cls):
         
@@ -16,8 +19,12 @@ class ProductInfo:
         ch = http.Http()
         # hard code a upc for now
         upc = '062020005632' #'4002293401102'
-        lookup = urlparse(f'https://api.upcitemdb.com/prod/trial/lookup?upc={upc}')
-        resp, content = ch.request(lookup.geturl(), 'GET', '', headers)
+        resp, content = ch.request(
+            f'https://api.upcitemdb.com/prod/trial/lookup?upc={upc}', 
+            'GET',
+            None,
+            headers
+        )
         data = json.loads(content)
 
         if data['code'] == 'OK':
@@ -30,14 +37,27 @@ class ProductInfo:
                 #search another api
                 walmart_upc = '035000521019'
                 api_key = '4n29ferqah8jjbatzb7v2vgw'
-                walmart_lookup = urlparse(f'http://api.walmartlabs.com/v1/items?apiKey={api_key}&upc={walmart_upc}')
-                resp, content = ch.request(walmart_lookup.geturl(), 'GET', headers)
+                resp, content = ch.request(
+                    f'http://api.walmartlabs.com/v1/items?apiKey={api_key}&upc={walmart_upc}',
+                    'GET',
+                    None,
+                    headers
+                )
                 walmart_data = json.loads(content)
-                print(walmart_data)      
-            
+                walmart_item = walmart_data.get('items')[0]
+                name = walmart_item.get('name')
+                sku = walmart_item.get('upc')
+                price = walmart_item.get('salePrice')
+                description_with_tags = walmart_item.get('shortDescription')
+                unescaped = html.parser.unescape(description_with_tags)
+                description = strip_tags(unescaped)
+                image = walmart_item.get('largeImage')
+
+                return name, sku, price, description, image              
+
         elif data['code'] == 'INVALID_UPC':
             # tell client to manually enter info
-            cls.data = {'code': 'not found'}     
+            cls.data = {'code': 'not found'}
 
 
     @classmethod
@@ -54,3 +74,4 @@ class ProductInfo:
 
         return name, sku, price, description, image
 
+ProductInfo.fetch_product_info()
