63a41705ab08a99853e03f00c26aed10ea0b2fe1
Rachael Topp <topp@ualberta.ca>
2017-11-27 14:35:09 -0500

    added path to user wishlists

--- gifts/urls.py
+++ gifts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import GiftsView, search
+from .views import GiftsView, search, getUserWishlists
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url(r'^(\d+)?$', csrf_exempt(GiftsView.as_view())),
-    url(r'^search/(\w+)$', search)
+    url(r'^wishlists/', getUserWishlists),
+    url(r'^search/(\w+)$', search),
 ]
