cd31bb1b0621eb1e6a40143f22d9e56584d78263
Rachael Topp <topp@ualberta.ca>
2017-11-28 12:54:37 -0500

    added endpoint for wishlist gifts

--- gifts/urls.py
+++ gifts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import GiftsView, search, getUserWishlists, postSearchGiftToGifts
+from .views import GiftsView, search, getUserWishlists, getWishlistGifts, postSearchGiftToGifts
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url(r'^(\d+)?$', csrf_exempt(GiftsView.as_view())),
+    url(r'^wishlists/(\d+)$', getWishlistGifts),
     url(r'^wishlists/', getUserWishlists),
     url(r'^add/', postSearchGiftToGifts),
     url(r'^search/(\w+)$', search),
